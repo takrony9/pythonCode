9b28f64feecfdafece18a3b51ab0bafce8ac1a81
takrony9 <takronymohamed@gmail.com>
2022-05-23 00:23:00 +0200

    Initial Commitfff5dxsdsdsaavfcdd

--- app.py
+++ app.py
@@ -69,9 +69,10 @@ def extract_ara_num(img1):
       count = count + 1
       # pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.1.0/bin/tesseract'
 
+
       img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
       th, img = cv2.threshold(img, 100, 255, cv2.THRESH_TRUNC)
-      pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Micro Systems\AppData\Local\Tesseract-OCR\\tesseract.exe'
+      pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Micro Systems\AppData\Local\Tesseract-OCR\tesseract.exe'
       res = pytesseract.image_to_string(img,lang="ara_t12").split()
       # print(res)
       if res != []:
