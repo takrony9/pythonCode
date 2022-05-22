8eaff2445a3085fab42eb33e94ae3ce2c272116c
yousseffinz <youssef@finzmoney.com>
2022-05-22 02:40:08 +0200

    Initial Commit final isa final

--- app.py
+++ app.py
@@ -51,17 +51,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def call():
    if request.method == 'POST':
       file = request.files['image']
-      with open('./model.bin', 'rb') as f_in:
-         model = pickle.load(f_in)
-         f_in.close()
+      # with open('./model.bin', 'rb') as f_in:
+      #    model = pickle.load(f_in)
+      #    f_in.close()
       filename = secure_filename(file.filename)
       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
       img = cv2.imread(filename)
       ara_num_res = extract_ara_num(img)
-      # number = unidecode(ara_num_res)
-      # os.remove(filename)
+      number = unidecode(ara_num_res)
+      os.remove(filename)
       result = {
-         'id_number':  ara_num_res
+         'id_number':  number
       }
       return jsonify(result)
 #       # cv2.waitKey(0)
