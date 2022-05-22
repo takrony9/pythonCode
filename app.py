f2d85af94454cd6a96e6a97b0eba8b3474a8d26e
yousseffinz <youssef@finzmoney.com>
2022-05-22 17:26:50 +0200

    startddfdfg

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def extract_ara_num(img1):
       count = count + 1
       img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
       th, img = cv2.threshold(img, 100, 255, cv2.THRESH_TRUNC)
-      res = pytesseract.image_to_string(img, lang="ara_t12").split()
+      res = pytesseract.image_to_string(img,"ara_t12").split()
       # print(res)
       if res != []:
          for i in res:
